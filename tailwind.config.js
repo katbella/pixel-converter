0b6ae504fd4bfc4afe7e3ebec8ea8b4b41bcb89d
Kat Bella <kat@katbella.com>
2025-04-13 18:46:17 -0700

    Feature/dark mode (#3)

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,7 @@
-import colors from "tailwindcss/dist/colors.js";
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
-  theme: {
-    extend: {
-      colors: {
-        background: colors.neutral[900],
-        foreground: colors.neutral[100],
-      },
-    },
-  },
+  theme: {},
   plugins: [],
   darkMode: "media",
 };
